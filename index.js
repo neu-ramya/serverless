990da28b9775cf0bf65bbe7f492b8f446c65e34d
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-11-24 23:24:53 -0500

    minor refactor

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ exports.handler = async function (event, context) {
 
   const ddb = new DynamoDBClient({ region: "us-east-1" });
 
-  console.log("********** IN HANDLER **********");
   const params = {
     TableName: process.env.tableName,
     Item: {
@@ -17,11 +16,9 @@ exports.handler = async function (event, context) {
 
   const command = new PutItemCommand(params);
   try {
-    console.log("------- INSERTING ------");
     const data = await ddb.send(command);
     console.log("Item added: ", data);
   } catch (err) {
-    console.log("------- ERRRRORRRRRRR ------");
     console.error(err);
   }
 };
