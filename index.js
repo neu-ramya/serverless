7ae4a8f9983df050c74e0dae792dd0e2ec982b83
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-11-24 23:22:53 -0500

    working connection to dynamodb

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("success");
+exports.handler = async function (event, context) {
+  const {
+    DynamoDBClient,
+    PutItemCommand,
+  } = require("@aws-sdk/client-dynamodb");
+
+  const ddb = new DynamoDBClient({ region: "us-east-1" });
+
+  console.log("********** IN HANDLER **********");
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      id: { S: "asdb" },
+      emailaddress: { S: "testing@gmail.com" },
+    },
+  };
+
+  const command = new PutItemCommand(params);
+  try {
+    console.log("------- INSERTING ------");
+    const data = await ddb.send(command);
+    console.log("Item added: ", data);
+  } catch (err) {
+    console.log("------- ERRRRORRRRRRR ------");
+    console.error(err);
+  }
+};
