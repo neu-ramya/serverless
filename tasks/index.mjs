143892a22ad5b42ad182e9d0fccafbbcab097e3b
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-11-26 12:17:06 -0500

    send mail and gcp

--- tasks/index.mjs
+++ tasks/index.mjs
@@ -2,12 +2,17 @@ import uploadToGCP from "./gcp.mjs";
 
 export const handler = async (event, context) => {
   try {
-    console.log("1. Starting handler");
-    const gcpResponse = await uploadToGCP(
-      "https://codeload.github.com/tparikh/myrepo/zip/refs/tags/v1.0.0"
-    );
+    console.log("Received event:", JSON.stringify(event, null, 2));
 
-    console.log('COMPLETING LAMBDA EXECUTION');
+    event.Records.forEach(async(record) => {
+      const message = JSON.parse(record.Sns.Message);
+      
+      console.log("SNS Message:", message);
+      console.log("DOWNLOAD URL:", message.url);
+      console.log("1. Starting handler");
+      const gcpResponse = await uploadToGCP(message.url);
+      console.log('COMPLETING LAMBDA EXECUTION');
+    });
   } catch (error) {
     console.error("Error in handler:", error);
   }
